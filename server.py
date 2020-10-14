e7bc2d3f75fd4904d2f3166de9519b3cf562728b
Ethan <ethancwh@gmail.com>
2020-10-14 15:53:59 +0800

    showed name from url parameter in template

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-   return render_template('index.html') 
-
-@app.route('/user/<username>')
-def show(username):
-    return f"Hi {username[0:3]}"
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name1=name)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
