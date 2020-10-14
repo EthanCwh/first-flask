3e8b3ad2cc8b25960a88f0dd093321aef4f17dca
Ethan <ethancwh@gmail.com>
2020-10-14 16:14:30 +0800

    added sign in and sign out button

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/<name>")
-def index(name):
-    name = name.upper()
-    return render_template('index.html', name1=name)
+
+@app.route("/")
+def index():
+    signed_in = True # we are hardcoding this just to demonstrate how we can do conditionals in our template files, in future we won't be hardcoding this.
+    return render_template('index.html', signed_in=signed_in)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
